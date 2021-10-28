76187e4de29452fbf48b4c460f65fb870362dd31
Gustavo-Sousa <gustavomotax2@gmail.com>
2021-10-28 20:35:00 -0300

    first commit

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:1337/parse"
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 instance.defaults.headers.common['X-Parse-Application-Id'] = "voosce";
